3edb30bb33c83e10eed8f32fd4d7764206e47712
Ian Whalen <ian.whalen@gmail.com>
2011-04-27 12:53:22 -0400

    newGame being called with event listener

--- bg.js
+++ bg.js
@@ -12,6 +12,7 @@ goog.require('lime.animation.MoveBy');
 goog.require('bg.Board');
 goog.require('bg.Game');
 goog.require('bg.EvadeOrFight');
+goog.require('bg.events');
 
 // set constant size
 bg.WIDTH = 1200;
@@ -27,30 +28,23 @@ bg.start = function() {
 
 // load menu scene
 bg.charactorSelect = function() {
-  var scene = new lime.Scene(),
-          layer = new lime.Layer().setPosition(bg.WIDTH / 2, 0);
+    bg.charSelectScene = new lime.Scene();
+    layer = new lime.Layer().setPosition(bg.WIDTH / 2, 0);
 
-      if(bg.isBrokenChrome()) layer.setRenderer(lime.Renderer.CANVAS);
+    if(bg.isBrokenChrome()) layer.setRenderer(lime.Renderer.CANVAS);
 
+    var charSelectBtns = new lime.Layer().setPosition(0, 430);
+    layer.appendChild(charSelectBtns);
 
-      var charSelectBtns = new lime.Layer().setPosition(0, 430);
-      layer.appendChild(charSelectBtns);
-      var move = new lime.animation.MoveBy(-bg.WIDTH, 0).enableOptimizations();
+    var lbl = new lime.Label().setText('Choose a Character to Play:').setFontColor('#FFFFFF').setFontSize(24).setPosition(0, 50);
+    var charSelect1 = bg.makeCharSelectBtn('Charactor 1').setPosition(0, 200);
+    goog.events.listen(charSelect1, 'click', function() { bg.newgame('char1') });
+    charSelectBtns.appendChild(charSelect1);
+    charSelectBtns.appendChild(lbl);
 
-      var lbl = new lime.Label().setText('Choose a Character to Play:').
-        setFontColor('#FFFFFF').
-        setFontSize(24).
-        setPosition(0, 50);
-      var charSelect1 = bg.makeCharSelectBtn('Charactor 1').setPosition(0, 200);
-      goog.events.listen(charSelect1, 'click', function() {
-          bg.newgame('char1')
-      });
-      charSelectBtns.appendChild(charSelect1);
-      charSelectBtns.appendChild(lbl);
+    bg.charSelectScene.appendChild(layer);
 
-      scene.appendChild(layer);
-
-      bg.director.replaceScene(scene, lime.transitions.Dissolve);
+    bg.director.replaceScene(bg.charSelectScene, lime.transitions.Dissolve);
 };
 
 // helper for char select buttons
@@ -63,10 +57,15 @@ bg.isBrokenChrome = function() {
     return (/Chrome\/9\.0\.597/).test(goog.userAgent.getUserAgentString());
 };
 
+
 // load new game scene
 bg.newgame = function(charactor) {
-    var scene = new bg.Game(charactor);
-    bg.director.replaceScene(scene, lime.transitions.Dissolve);
+    bg.gameScene = new bg.Game(charactor);
+    var transition = bg.director.replaceScene(bg.gameScene, lime.transitions.Dissolve);
+
+    goog.events.listen(transition, 'end', function() {
+        lime.scheduleManager.callAfter(bg.gameScene.drawMythosCard(), this, 100);
+    });
 };
 
 
