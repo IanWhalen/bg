861c8af40c17e6fad0f21fb5286e9d3fb518df50
Ian Whalen <ian.whalen@gmail.com>
2011-04-29 14:36:17 -0400

    Dice rolling properly in evade stage

--- evadeOrFight.js
+++ evadeOrFight.js
@@ -1,7 +1,6 @@
 goog.provide('bg.EvadeOrFight');
 
 goog.require('lime.GlossyButton');
-goog.require('bg.evade_skillCheck');
 
 
 /**
@@ -15,21 +14,24 @@ bg.EvadeOrFight = function(charactor, foe) {
 
     this.charactor = charactor;
     this.foe = foe;
+    this.successes = 0;
 
-    this.choice = new lime.Sprite().setSize(800, 600).setFill('#CCC').setPosition(200, 200).setAnchorPoint(0, 0);
+    this.upperPane = new lime.Sprite().setSize(800, 300).setFill('#CCC').setPosition(200, 200).setAnchorPoint(0, 0);
+    this.lowerPane = new lime.Sprite().setSize(800, 300).setFill('#CCC').setPosition(200, 500).setAnchorPoint(0, 0);
 
     var char_image = new lime.Sprite().setFill(charactor.circle.fill_.url_).setPosition(200, 200);
     var foe_image = new lime.Sprite().setFill(foe.circle.fill_.url_).setPosition(600, 200);
-    this.choice.appendChild(char_image);
-    this.choice.appendChild(foe_image);
+    this.upperPane.appendChild(char_image);
+    this.upperPane.appendChild(foe_image);
 
-    this.btn_evade = new lime.GlossyButton('Evade!').setPosition(300, 500).setSize(100, 100);
-    this.btn_fight = new lime.GlossyButton('Fight!').setPosition(500, 500).setSize(100, 100);
-    this.choice.appendChild(this.btn_evade);
-    this.choice.appendChild(this.btn_fight);
+    this.btn_evade = new lime.GlossyButton('Evade!').setPosition(300, 200).setSize(100, 100);
+    this.btn_fight = new lime.GlossyButton('Fight!').setPosition(500, 200).setSize(100, 100);
+    this.lowerPane.appendChild(this.btn_evade);
+    this.lowerPane.appendChild(this.btn_fight);
 
     if (bg.isBrokenChrome()) this.choice.setRenderer(lime.renderer.CANVAS);
-    this.appendChild(this.choice);
+    this.appendChild(this.upperPane);
+    this.appendChild(this.lowerPane);
     
     goog.events.listen(this.btn_evade, 'click', function() {
         this.scene_.evade();
@@ -43,18 +45,35 @@ goog.inherits(bg.EvadeOrFight, lime.Scene);
 
 
 bg.EvadeOrFight.prototype.evade = function() {
-    this.choice.removeChild(this.btn_fight);
-    this.choice.removeChild(this.btn_evade);
+    // Clear section of buttons
+    this.lowerPane.removeAllChildren();
+
+    // Determine # of die to display
     var d = this.charactor.sneak + this.foe.awareness;
-    var instruct = new lime.Label('Click squares to roll die:').setPosition(400, 400);
-    this.choice.appendChild(instruct);
+
+    // Create die objects and label
+    var instruct = new lime.Label('Click squares to roll die:').setPosition(400, 100);
+    this.lowerPane.appendChild(instruct);
     for (i=0; i < d; i++) {
-        var btn_die = new lime.GlossyButton('?').setPosition(200 + i * 100, 500).setSize(50, 50).setColor('#FFF');
-        this.choice.appendChild(btn_die);
+        var btn_die = new lime.GlossyButton('?').setPosition(200 + i * 100, 200).setSize(50, 50).setColor('#FFF');
+        this.lowerPane.appendChild(btn_die);
         goog.events.listenOnce(btn_die, 'click', function() {
-            rollResult = Math.floor(Math.random() * 6 + 1);
-            this.setText(rollResult);
-            (rollResult > 4 ? this.setColor('#008000') : this.setColor('#FF0000'));
-        })
+          this.scene_.rollDie(this)
+    });
     }
+
 };
+
+
+// Roll die and change display accordingly
+bg.EvadeOrFight.prototype.rollDie = function(btn_die) {
+    var rollResult = Math.floor(Math.random() * 6 + 1);
+    btn_die.setText(rollResult);
+    if (rollResult > 4) {
+        btn_die.setColor('#008000');
+        btn_die.scene_.successes += 1;
+    } else {
+        btn_die.setColor('#FF0000');
+    }
+
+}
