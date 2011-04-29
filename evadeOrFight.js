ae0636a8110fa0c77cc457b28dc9d12ab25206ee
Ian Whalen <ian.whalen@gmail.com>
2011-04-29 17:10:30 -0400

    Character only moves after evade is successful

--- evadeOrFight.js
+++ evadeOrFight.js
@@ -49,12 +49,12 @@ bg.EvadeOrFight.prototype.evade = function() {
     this.lowerPane.removeAllChildren();
 
     // Determine # of die to display
-    var d = this.charactor.sneak + this.foe.awareness;
+    this.lowerPane.d = this.charactor.sneak + this.foe.awareness;
 
     // Create die objects and label
     var instruct = new lime.Label('Click squares to roll die:').setPosition(400, 100);
     this.lowerPane.appendChild(instruct);
-    for (i=0; i < d; i++) {
+    for (i=0; i < this.lowerPane.d; i++) {
         var btn_die = new lime.GlossyButton('?').setPosition(200 + i * 100, 200).setSize(50, 50).setColor('#FFF');
         this.lowerPane.appendChild(btn_die);
         goog.events.listenOnce(btn_die, 'click', function() {
@@ -68,7 +68,10 @@ bg.EvadeOrFight.prototype.evade = function() {
 // Roll die and change display accordingly
 bg.EvadeOrFight.prototype.rollDie = function(btn_die) {
     var rollResult = Math.floor(Math.random() * 6 + 1);
+
+    // Change die based on result
     btn_die.setText(rollResult);
+    this.lowerPane.d -= 1;
     if (rollResult > 4) {
         btn_die.setColor('#008000');
         btn_die.scene_.successes += 1;
@@ -76,4 +79,21 @@ bg.EvadeOrFight.prototype.rollDie = function(btn_die) {
         btn_die.setColor('#FF0000');
     }
 
+    // If all dice are rolled, move to board or fight scene
+    if (!this.lowerPane.d) {
+        if (this.successes) {
+            var transition = bg.director.replaceScene(bg.gameScene, lime.transitions.SlideInDown);
+
+            goog.events.listen(transition, 'end', function() {
+                bg.gameScene.board.dispatchEvent('char_move_safe');
+            });
+        } else {
+            this.fight();
+        }
+    }
+
+}
+
+
+bg.EvadeOrFight.prototype.fight = function() {
 }
