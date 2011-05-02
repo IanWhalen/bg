89e5bcdd6e2b6d3c759dd4e2ea31de8e02251864
Ian Whalen <ian.whalen@gmail.com>
2011-05-01 21:50:10 -0400

    Combat checks now implemented

--- evadeOrFight.js
+++ evadeOrFight.js
@@ -1,6 +1,7 @@
 goog.provide('bg.EvadeOrFight');
 
 goog.require('bg.HorrorCheck');
+goog.require('bg.EvadeCheck');
 goog.require('lime.GlossyButton');
 
 
@@ -35,7 +36,7 @@ bg.EvadeOrFight = function(charactor, foe) {
     this.appendChild(this.lowerPane);
     
     goog.events.listen(this.btn_evade, 'click', function() {
-        this.scene_.evade();
+        var check = new bg.EvadeCheck(this.scene_);
     });
     goog.events.listen(this.btn_fight, 'click', function() {
         var check = new bg.HorrorCheck(this.scene_); 
@@ -43,54 +44,3 @@ bg.EvadeOrFight = function(charactor, foe) {
 
 }
 goog.inherits(bg.EvadeOrFight, lime.Scene);
-
-
-bg.EvadeOrFight.prototype.evade = function() {
-    // Clear section of buttons
-    this.lowerPane.removeAllChildren();
-
-    // Determine # of die to display
-    this.lowerPane.d = this.charactor.sneak + this.foe.awareness;
-
-    // Create die objects and label
-    var instruct = new lime.Label('Click squares to roll die for Evade Check:').setPosition(400, 100);
-    this.lowerPane.appendChild(instruct);
-    for (i=0; i < this.lowerPane.d; i++) {
-        var btn_die = new lime.GlossyButton('?').setPosition(200 + i * 100, 200).setSize(50, 50).setColor('#FFF');
-        this.lowerPane.appendChild(btn_die);
-        goog.events.listenOnce(btn_die, 'click', function() {
-            this.scene_.rollDie(this);
-        });
-    }
-
-};
-
-
-// Roll die and change display accordingly
-bg.EvadeOrFight.prototype.rollDie = function(btn_die) {
-    var rollResult = Math.floor(Math.random() * 6 + 1);
-
-    // Change die based on result
-    btn_die.setText(rollResult);
-    this.lowerPane.d -= 1;
-    if (rollResult > 4) {
-        btn_die.setColor('#008000');
-        btn_die.scene_.successes += 1;
-    } else {
-        btn_die.setColor('#FF0000');
-    }
-
-    // If all dice are rolled, move to board or fight scene
-    if (!this.lowerPane.d) {
-        if (this.successes) {
-            var transition = bg.director.replaceScene(bg.gameScene, lime.transitions.SlideInDown);
-
-            goog.events.listen(transition, 'end', function() {
-                bg.gameScene.board.dispatchEvent('char_move_safe');
-            });
-        } else {
-            var check = new bg.HorrorCheck(this.scene_); 
-        }
-    }
-
-}
--- skillCheck.js
+++ skillCheck.js
@@ -1,4 +1,5 @@
 goog.provide('bg.HorrorCheck');
+goog.provide('bg.EvadeCheck');
 
 
 bg.SkillCheck = function(scene) {
@@ -12,12 +13,12 @@ bg.SkillCheck.prototype.setInfoLabel = function(text) {
 
 
 bg.SkillCheck.prototype.setDice = function(d) {
-    for (i=0; i < d; i++) {
+    for (var i=0; i < d; i++) {
         var btn_die = new lime.GlossyButton('?').setPosition(200 + i * 100, 200).setSize(50, 50).setColor('#FFF');
         this.scene.lowerPane.appendChild(btn_die);
-        goog.events.listenOnce(btn_die, 'click', function() {
-            this.rollDie(btn_die);
-        }, false, this);
+        goog.events.listenOnce(btn_die, 'click', (function(btn) {
+            return function() { this.rollDie(btn); }
+        })(btn_die), false, this);
     }
     
 }
@@ -36,7 +37,41 @@ bg.SkillCheck.prototype.rollDie = function(btn_die) {
         btn_die.setColor('#FF0000');
     }
 
-    this.testForLastDie();
+    if (!this.d) this.afterLastDie();
+}
+
+
+bg.EvadeCheck = function(scene) {
+    // Clear section of buttons
+    this.scene = scene;
+    scene.lowerPane.removeAllChildren();
+
+    // Determine # of die to display
+    this.d = scene.charactor.sneak + scene.foe.awareness;
+    this.successes = 0;
+
+    // Create label
+    this.setInfoLabel('Click squares to roll die for Evade Check:');
+    
+    // Setup dice to roll
+    this.setDice(this.d);
+}
+goog.inherits(bg.EvadeCheck, bg.SkillCheck);
+
+
+bg.EvadeCheck.prototype.afterLastDie = function() {
+    // If all dice are rolled, move to board or fight scene
+    if (this.successes) {
+        var transition = bg.director.replaceScene(bg.gameScene, lime.transitions.SlideInDown);
+        goog.events.listen(transition, 'end', function() {
+            bg.gameScene.board.dispatchEvent('char_move_safe');
+        });
+
+    } else {
+        this.scene.charactor.health -= this.scene.foe.combatDamage;
+        var check = new bg.HorrorCheck(this.scene); 
+    }
+
 }
 
 
@@ -50,7 +85,7 @@ bg.HorrorCheck = function(scene) {
     this.successes = 0;
 
     // Create label
-    this.setInfoLabel('Click squares to roll die for Horror Check:', scene);
+    this.setInfoLabel('Click squares to roll die for Horror Check:');
 
     // Setup dice to roll
     this.setDice(this.d);
@@ -59,15 +94,47 @@ bg.HorrorCheck = function(scene) {
 goog.inherits(bg.HorrorCheck, bg.SkillCheck);
 
 
-bg.HorrorCheck.prototype.testForLastDie = function() {
+bg.HorrorCheck.prototype.afterLastDie = function() {
     // If all dice are rolled, move to board or fight scene
-    if (!this.d) {
-        if (this.successes) {
-            // bg.FightCheck();
-        } else {
-            this.scene.charactor.sanity -= this.scene.foe.horrorDamage;
-            // bg.FightCheck();
-        }
+    if (this.successes) {
+        var check = new bg.FightCheck(this.scene);
+    } else {
+        this.scene.charactor.sanity -= this.scene.foe.horrorDamage;
+        var check = new bg.FightCheck(this.scene);
     }
+}
 
+
+bg.FightCheck = function(scene) {
+    // Clear section of buttons
+    this.scene = scene;
+    scene.lowerPane.removeAllChildren();
+
+    // Determine # of die to display
+    this.d = scene.charactor.fight + scene.foe.combatRating;
+    this.successes = 0;
+
+    // Create label
+    this.setInfoLabel('Click squares to roll die for Fight Check:');
+
+    // Setup dice to roll
+    this.setDice(this.d);
+}
+goog.inherits(bg.FightCheck, bg.SkillCheck);
+
+
+bg.FightCheck.prototype.afterLastDie = function() {
+    if (this.successes) {
+        this.scene.foe.setHidden(true);
+        this.scene.charactor.trophyStack.push(this.scene.foe); 
+
+        var transition = bg.director.replaceScene(bg.gameScene, lime.transitions.SlideInDown);
+        goog.events.listen(transition, 'end', function() {
+            bg.gameScene.board.dispatchEvent('char_move_safe');
+        });
+    
+    } else {
+        this.scene.charactor.health -= this.scene.foe.combatDamage;
+        var check = new bg.FightCheck(this.scene);
+    }
 }
